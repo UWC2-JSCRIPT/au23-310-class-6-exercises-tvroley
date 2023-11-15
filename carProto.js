3038d0871f57ec144d25a3b3b70eb1a1528bc6a8
Theodore Roley <tvroley@uw.edu>
2023-11-14 19:50:14 -0800

    Car class

--- carProto.js
+++ carProto.js
@@ -3,8 +3,30 @@
  * @constructor
  * @param {String} model
  */
+class Car {
+    constructor(model) {
+        this.model = model;
+        this.currentSpeed = 0;
+
+        this.accelerate = () => {
+            this.currentSpeed++;
+        }
+
+        this.brake = () => {
+            this.currentSpeed--;
+        }
+    }
+    toString() {
+        return `${this.model} is going ${this.currentSpeed}`;
+    }
+}
 
 //  Create an instance, accelerate twice, brake once, and console log the instance.toString()
+const geo = new Car('Geo');
+geo.accelerate();
+geo.accelerate();
+geo.brake();
+console.log(geo.toString());
 
 /**
  * ElectricCar class
