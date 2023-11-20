8f4e34b0d467b49a7f97768c5209c16ce8baa8af
Theodore Roley <tvroley@uw.edu>
2023-11-19 18:18:33 -0800

    added jquery code that waits for page to load

--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -13,3 +13,8 @@
 // Create a listener on the "Buy Now!" link that responds to a click event.
 // When clicked, the the "Buy Now!" link should be removed
 // and replaced with text that says "Added to cart"
+$( document ).ready(function() {
+    // All of your code in here
+    // This uses jQuery, but there is a plain
+    // JavaScript way to do this as well
+    });
