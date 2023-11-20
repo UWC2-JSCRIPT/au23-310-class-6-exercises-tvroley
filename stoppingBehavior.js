759e925a014364871fab6d6dc7ec7ba0b84d15c4
Theodore Roley <tvroley@uw.edu>
2023-11-19 23:00:51 -0800

    stopping behavior

--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -5,6 +5,16 @@ document.getElementById('cat').addEventListener('click', () => {
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
+const $a = $('a');
+$a.click(function(e) {
+  e.preventDefault();
+  alert(`Here's some info`);
+});
 
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+const $button = $('button');
+$button.click(function(e) {
+  e.stopPropagation();
+  alert('Bow wow!');
+});
